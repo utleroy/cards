489ee8be84422652b5a4964da45902e3fb2551e8
utleroy <bryanhankins@me.com>
2016-03-05 12:42:57 -0600

    added variables in js

---
+++ cards.js
@@ -0,0 +1,5 @@
+var create = document.getElementById("create");
+var erase = document.getElementById("erase");
+var input = document.getElementById("input");
+var container = document.getElementById("container");
+var card = document.getElementsByClass("card");
