655ceaae9d173e7b205f2df9183d876d19b580ba
utleroy <bryanhankins@me.com>
2016-03-09 18:52:59 -0600

    finished all functionality

--- cards.js
+++ cards.js
@@ -4,18 +4,27 @@ var input = document.getElementById("input");
 var container = document.getElementById("container");
 var card = document.getElementsByClassName("card"); //[this returns an array always]
 
-
+create.addEventListener("click", createCard);
 
 function createCard() {
   var output = '<div class="card"><section><p class="text">' + input.value + '</p></section><input type="button" class="delete" value="Delete"></div>'
           container.innerHTML += output;
+          deleteBtnEventListener();
 }
 
-function deleteBtnEl() { 
-  input.delete; 
-		 {
-		};
-	}
+function deleteBtn(event) {
+   var thisCard = event.target.parentElement;
+  container.removeChild(thisCard)
+}
 
-create.addEventListener("click", createCard);
-erase.addEventListener("click", deleteBtnEl);
+function deleteBtnEventListener() {
+	for(var i = 0; i < erase.length; i++) {
+		var gone = document.getElementsByClassName("delete")
+		   for( var j = 0; j < gone.length; j++)
+		   	gone[j].addEventListener("click", deleteBtn)
+	}
+}
+
+
+
+
